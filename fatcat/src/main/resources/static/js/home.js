9872077ceaa0e0a3ad63ad319486eb9936e9312c
sqweet <tyz0001@163.com>
2018-10-31 16:09:03 +0800

    C:\Users\Sweeeeeeeeet>c

--- fatcat/src/main/resources/static/js/home.js
+++ fatcat/src/main/resources/static/js/home.js
@@ -1,12 +1,12 @@
 window.onload=function(){
-    var images=["img/1.jpeg","img/2.jpeg","img/4.jpeg","img/5.jpeg"];
+    var images=["img/1.jpeg","img/2.jpeg","img/3.jpg","img/4.jpeg","img/5.jpeg"];
     var news=document.getElementById("news");
     news.style.background="url('img/1.jpeg')";
     var i=1;
     function duration() {
         news.style.background="url("+images[i]+")";
         i++;
-        i=i%4;
+        i=i%5;
     }
         setInterval(duration,3500)
 }
@@ -14,15 +14,19 @@ function pic1(){
     news.style.background="url('img/1.jpeg')";
     i=1;
 }
-function pic1(){
-    news.style.background="url('img/1.jpeg')";
-    i=1;
+function pic2(){
+    news.style.background="url('img/2.jpeg')";
+    i=2;
 }
-function pic1(){
-    news.style.background="url('img/1.jpeg')";
-    i=1;
+function pic3(){
+    news.style.background="url('img/3.jpg')";
+    i=3;
 }
-function pic1(){
-    news.style.background="url('img/1.jpeg')";
-    i=1;
+function pic4(){
+    news.style.background="url('img/4.jpeg')";
+    i=4;
+}
+function pic5(){
+    news.style.background="url('img/5.jpeg')";
+    i=5;
 }
